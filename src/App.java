5067852796c8c121b542d92374c9ba4391d8fb98
raulFuzita <raul_fuzita@hotmail.com>
2021-05-18 03:45:05 +0100

    Simple example of how to load an image

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
 import java.awt.*;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class App extends JFrame {
+
+    private Screen s;
+    private Image bg;
+    private Image emoji;
+    private boolean loaded = false;
     public static void main(String[] args) throws Exception {
         
         DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
@@ -14,10 +20,11 @@ public class App extends JFrame {
         setForeground(Color.WHITE);
         setFont(new Font("Arial", Font.PLAIN, 24));
 
-        Screen s = new Screen();
+        s = new Screen();
 
         try {
             s.setFullScreen(dm, this);
+            loadpics();
             Thread.sleep(5000);
         } catch (Exception e) {
             e.printStackTrace();
@@ -25,6 +32,14 @@ public class App extends JFrame {
             s.restoreScreen();
         }
         
+    }
+
+    // loads pictures
+    public void loadpics() {
+        bg = new ImageIcon("resource//images//background.jpg").getImage();
+        emoji = new ImageIcon("resource//images//smiling.png").getImage();
+        loaded = true;
+        repaint();
     }
 
     // This method is called by JFrame automatically
@@ -36,5 +51,10 @@ public class App extends JFrame {
             g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         }
         g.drawString("This is gonna be awesome", 200, 200);
+
+        if (loaded) {
+            g.drawImage(bg, 0, 0, null);
+            g.drawImage(emoji, 100, 50, null);
+        }
     }
 }
