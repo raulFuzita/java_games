90ca39d325ad60a566972b0ec544ce76d0d63678
raulFuzita <raul_fuzita@hotmail.com>
2021-05-18 21:29:23 +0100

    Creating modes and a simple Sprite class

--- src/App.java
+++ src/App.java
@@ -5,65 +5,77 @@ import javax.swing.JFrame;
 
 public class App extends JFrame {
 
-    private Screen screen;
-    private Image bg;
     private Animation a;
+    private ScreenManager s;
+    private Image bg;
+
+    private static final DisplayMode modes1[] = {
+        new DisplayMode(800, 600, 32, 0),
+        new DisplayMode(800, 600, 24, 0),
+        new DisplayMode(800, 600, 16, 0),
+        new DisplayMode(640, 480, 32, 0),
+        new DisplayMode(640, 480, 24, 0),
+        new DisplayMode(640, 480, 16, 0)
+    };
     public static void main(String[] args) throws Exception {
-        
-        DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
         App app = new App();
-        app.run(dm);
+        app.run();
+    }
+    
+    // load images and add scenes
+    public void loadImages() {
+        bg = new ImageIcon("resource//images//background.jpg").getImage();
+        Image face1 = new ImageIcon("resource//images//normal.png").getImage();
+        Image face2 = new ImageIcon("resource//images//smiling.png").getImage();
+
+        a = new Animation();
+        a.addScene(face1, 250);
+        a.addScene(face2, 250);
     }
 
-    public void run(DisplayMode dm) {
-        screen = new Screen();
+    // main method called from main
+    public void run() {
+        s = new ScreenManager();
         try {
-            screen.setFullScreen(dm, new JFrame());
-            loadPics();
+            DisplayMode dm = s.findFirstCompatibleMode(modes1);
+            s.setFullScreen(dm);
+            loadImages();
             movieLoop();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            screen.restoreScreen();
+            s.restoreScreen();
         }
     }
 
-    // loads pictures computer to java and adds scene
-    public void loadPics() {
-        bg = new ImageIcon("resource//images//background.jpg").getImage();
-        Image emoji1 = new ImageIcon("resource//images//normal.png").getImage();
-        Image emoji2 = new ImageIcon("resource//images//smiling.png").getImage();
-        Image emoji3 = new ImageIcon("resource//images//sunglasses.png").getImage();
-        a = new Animation();
-        a.addScene(emoji1, 250);
-        a.addScene(emoji2, 250);
-        a.addScene(emoji3, 250);
-    }
-
-    // main moview loop
+    // play movie
     public void movieLoop() {
         long startingTime = System.currentTimeMillis();
         long cumTime = startingTime;
-
-        while(cumTime - startingTime < 5000) {
+        while (cumTime - startingTime < 5000) {
             long timePassed = System.currentTimeMillis() - cumTime;
             cumTime += timePassed;
             a.update(timePassed);
 
-            Graphics g = screen.getFullScreenWindow().getGraphics();
+            // draw and update screen
+            Graphics2D g = s.getGraphics();
             draw(g);
             g.dispose();
+            s.update();
 
             try {
-                Thread.sleep(120);
+                Thread.sleep(20);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
+    // draws graphics
     public void draw(Graphics g) {
         g.drawImage(bg, 0, 0, null);
         g.drawImage(a.getImage(), 0, 0, null);
     }
+    
+
 }
---
+++ src/Sprite.java
@@ -0,0 +1,82 @@
+import java.awt.Image;
+
+public class Sprite {
+    
+    private Animation a;
+    private float x;
+    private float y;
+    private float vx;
+    private float vy;
+
+
+    public Sprite(Animation a, float x, float y, float vx, float vy) {
+        this.a = a;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    // chnage position
+    public void update(long timePassed) {
+        x += vx * timePassed;
+        y += vy * timePassed;
+        a.update(timePassed);
+    }
+
+    // get x position
+    public float getX() {
+        return x;
+    }
+
+    // get x position
+    public float getY() {
+        return y;
+    }
+
+    // set sprite x position
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    // set sprite x position
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    // get sprite width
+    public int getWidth() {
+        return a.getImage().getWidth(null);
+    }
+
+    // get sprite width
+    public int getHeight() {
+        return a.getImage().getHeight(null);
+    }
+
+    // get horizontal velocity
+    public float getVelocityX() {
+        return vx;
+    }
+
+    // get vertical velocity
+    public float getVelocityY() {
+        return vy;
+    }
+
+    // set horizonal velocity
+    public void setVelocityX(float vx) {
+        this.vx = vx;
+    }
+
+    // set horizonal velocity
+    public void setVelocityY(float vy) {
+        this.vy = vy;
+    }
+
+    // get sprite / image
+    public Image getImage() {
+        return a.getImage();
+    }
+
+}
