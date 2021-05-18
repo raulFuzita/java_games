1ffa539fae2e7f8b0557e151320297616aeef418
raulFuzita <raul_fuzita@hotmail.com>
2021-05-18 03:20:55 +0100

    Creating and restoring screen

--- src/App.java
+++ src/App.java
@@ -1,15 +1,40 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.awt.*;
+import javax.swing.JFrame;
 
-public class App {
+public class App extends JFrame {
     public static void main(String[] args) throws Exception {
         
-        ExecutorService es = Executors.newCachedThreadPool();
+        DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
+        App app = new App();
+        app.run(dm);
+    }
 
-        es.execute(new Apple("One"));
-        es.execute(new Apple("Two"));
-        es.execute(new Apple("Three"));
+    public void run(DisplayMode dm) {
+        getContentPane().setBackground(Color.PINK);
+        setForeground(Color.WHITE);
+        setFont(new Font("Arial", Font.PLAIN, 24));
 
-        es.shutdown();
+        Screen s = new Screen();
+
+        try {
+            s.setFullScreen(dm, this);
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            s.restoreScreen();
+        }
+        
+    }
+
+    // This method is called by JFrame automatically
+    public void paint(Graphics g) {
+        super.paint(g);
+        // Make the text smoth
+        if (g instanceof Graphics2D) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        }
+        g.drawString("This is gonna be awesome", 200, 200);
     }
 }
